6ea2e7302898f32073d793ae88d352e94f88ac55
Annabelhxy <xih035@ucsd.edu>
2024-10-14 16:09:15 -0700

    Task 2.5

--- new-app/my-app/src/toDoList.tsx
+++ new-app/my-app/src/toDoList.tsx
@@ -35,14 +35,14 @@ export function ToDoList() {
     }
 
     return (
-        <div className="App">
-        <div className="App-body">
-        <h1>{name}'s To Do List</h1>
-            Items bought: {numRemainingItems}
-            <form action=".">
-            {items.map((item) => ListItem(item, handleCheckboxClick))}
-            </form>
-        </div>
+        <div className="center-content">
+            <div className="App-body">
+                <h1>{name}'s To Do List</h1>
+                Items bought: {numRemainingItems}
+                <form action=".">
+                {items.map((item) => ListItem(item, handleCheckboxClick))}
+                </form>
+            </div>
         </div>
     );
 }
